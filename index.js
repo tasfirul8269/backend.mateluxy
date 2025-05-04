981a5dd724e8527e6ccb212167383dd2fbc22ce1
Tasfirul Haque <adef05675@gmail.com>
2025-05-05 01:59:37 +0600

    Auth

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'https://mateluxy-frontend-sudw.vercel.app',
     'https://real-state-frontend-sigma.vercel.app',
-    'https://frontend-mateluxy.vercel.app/'
+    'https://frontend-mateluxy.vercel.app'
   ];
   
   app.use(cors({
