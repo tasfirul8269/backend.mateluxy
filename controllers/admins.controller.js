6633315ca4396acb30fd765ebe520317248d7235
Tasfirul Haque <adef05675@gmail.com>
2025-05-06 12:32:20 +0600

    Agents

--- controllers/admins.controller.js
+++ controllers/admins.controller.js
@@ -1,3 +1,11 @@
-export const admins = (req, res) => {
-    res.send("Admins");
+import Admin from "../models/admin.model.js";
+import { errorHandler } from "../utils/erros.js";
+
+export const admins = async (req, res, next) => {
+    try {
+        const allAdmins = await Admin.find().select('-password');  // Exclude password field
+        res.status(200).json(allAdmins);
+    } catch (error) {
+        next(error);
+    }
 };
