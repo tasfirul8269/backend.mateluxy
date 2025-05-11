7bbc95d1ffa1899518ca2966ff34b7068a61ffd3
Tasfirul Haque <adef05675@gmail.com>
2025-05-11 23:56:40 +0600

    Remember me

--- controllers/adminSignIn.controller.js
+++ controllers/adminSignIn.controller.js
@@ -3,9 +3,8 @@ import { errorHandler } from "../utils/erros.js";
 import Admin from "../models/admin.model.js";
 import jwt from "jsonwebtoken";
 
-
 export const adminSignIn = async (req, res, next) => {
-    const { email, password } = req.body;
+    const { email, password, rememberMe } = req.body;
     try {
         const validAdmin = await Admin.findOne({ email });
         if (!validAdmin) {
@@ -16,15 +15,31 @@ export const adminSignIn = async (req, res, next) => {
             return next(errorHandler(401, "Invalid credentials"));
         }
         
+        // Create JWT token with admin ID
         const token = jwt.sign({ id: validAdmin._id }, process.env.JWT_SECRET);
 
+        // Remove password from response
         const { password: pass, ...rest } = validAdmin._doc;
-
-        res.cookie("access_token", token, {
+        
+        // Set cookie options based on rememberMe
+        const cookieOptions = {
             httpOnly: true,
             secure: true, // must be false on localhost (HTTP)
-            sameSite: "None" // or "strict", either is fine
-        }).status(200).json(rest);
+            sameSite: "None", // or "strict", either is fine
+        };
+        
+        // If rememberMe is true, set expiration to 30 days, otherwise session cookie
+        if (rememberMe) {
+            cookieOptions.expires = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // 30 days
+            console.log('Remember me enabled, setting cookie expiration to 30 days');
+        } else {
+            console.log('Remember me disabled, using session cookie');
+        }
+        
+        // Set cookie and return response
+        res.cookie("access_token", token, cookieOptions)
+           .status(200)
+           .json({ ...rest, rememberMe });
 
     } catch (error) {
         next(error);
